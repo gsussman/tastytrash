8fc02efeca8ec9aaf31f376144b4ccb9ad29688c
Gene Sussman <Gene.Sussman@ibm.com@Genes-MacBook-Pro.local>
2021-01-27 13:42:58 -0500

    update settings for digital ocean

--- tastytrash/settings.py
+++ tastytrash/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qa%rrbld%_8xo0(ps!a@i*40m2z^hxr9+=93nj8^9jgvc4!=er'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['157.245.127.74', 'localhost']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
